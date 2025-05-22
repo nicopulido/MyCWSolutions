816a695c55915f6d3800cd5a816943b51d052484
nicopulido <nicopipe2010@gmail.com>
2025-05-21 21:42:13 -0500

    fixed some bugs on extract the domain file

--- ExtractTheDomain.py
+++ ExtractTheDomain.py
@@ -1,16 +1,18 @@
 def domain_name(url):
-    i = 1
-    while url[-i] != '.':
+    url = url.replace("www.", "")
+    i = 0
+    while url[i] not in ('/','.'):
         i += 1
     j = i + 1
-    while url[-j] not in ('/','.'):
+    while url[j] != '.' and j < len(url) - 1:
         j += 1
+
+    return url[i + 2: j] if url[i] == '/' else url[:i]
     
-    return url[-j + 1:-i]
 
 
 #this is the url of the kata: https://www.codewars.com/kata/514a024011ea4fb54200004b/train/python
 
 #this is a example of the input
-print(domain_name("https://hyphen-site.org"))
+print(domain_name("www.xakep.ru"))
         
